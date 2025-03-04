3b3790e554d0e01e92278a32e10fadb516ed3b23
Maxim Tynyanko <data.expanged@gmail.com>
2025-03-05 00:27:22 +0300

    migrations

---
+++ websocket_app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from django.db import models
+
+class CryptoPrice(models.Model):
+    symbol = models.CharField(max_length=10)
+    price = models.DecimalField(max_digits=20, decimal_places=10)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.symbol}: {self.price} ({self.timestamp})"
